b35c21f896a633993e462de448944bb940b2f896
Hallett-K <krystalscarlett@outlook.com>
2026-09-14 14:03:11 +0100

    Stack and BSS init

--- kernel/arch/aarch64/boot.s
+++ kernel/arch/aarch64/boot.s
@@ -3,8 +3,27 @@
 .type _asm_start, %function
 
 _asm_start:
+
+    // Stack
+    ldr x0, =__stack_top
+    mov sp, x0
+
+    // Clear BSS
+    ldr x0, =__bss_start
+    ldr x1, =__bss_end
+
+bss_clear:
+    cmp x0, x1 
+    b.hs bss_cleared
+
+    str xzr, [x0]
+    add x0, x0, #8
+    b bss_clear
+
+bss_cleared:
     bl kernel_main
 
 1:
     wfe
     b 1b
+    
